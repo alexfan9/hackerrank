75ed45bc6636e5abbb6fe4f66477124e10bf19b4
alexfan9 <ftb_cyc@163.com>
2023-08-12 21:02:24 +0800

    Update query_the_document.c

--- query_the_document.c
+++ query_the_document.c
@@ -1,3 +1,7 @@
+/*
+Hanckerrank
+https://www.hackerrank.com/challenges/querying-the-document/problem?isFullScreen=true&h_r=next-challenge&h_v=zen 
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
