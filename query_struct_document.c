e9b5e5e8e9720c3ff6d4b152329626c20e006531
alexfan9 <ftb_cyc@163.com>
2023-08-13 18:59:27 +0800

    Update query_struct_document.c

--- query_struct_document.c
+++ query_struct_document.c
@@ -28,7 +28,6 @@ struct document {
     int paragraph_count;//denotes number of paragraphs in a document
 };
 struct document get_document(char* text) {
-    printf("get_document enter\n");
     char *p = text;
     int word_start = 0, word_end = 0;
     
@@ -45,74 +44,106 @@ struct document get_document(char* text) {
         if(*p == ' ' || *p == '.'){
             word_end = p - text;
             p_word = (struct word*)realloc(p_word, (word_count +1 )* sizeof(struct word*));
+            if(p_word == NULL) {
+                printf("realloc p_word fails\n");
+                return doc;
+            }
             char *p_data = calloc(word_end-word_start +1, sizeof(char));
+            if(p_data == NULL) {
+                printf("realloc p_data fails\n");
+                return doc;
+            }
             strncpy(p_data, text+word_start, word_end-word_start);
             p_word[word_count].data = p_data;
-            
-            if(p_sentence == NULL){
-                p_sentence = calloc(1, sizeof(struct sentence*));
-            }
-            printf("word_count=%d,p_word->data=%s\n", word_count, p_word[word_count].data);
             word_count ++;
-            printf("size=%ld\n", word_count * sizeof(struct word *));
-            p_sentence->data = (struct word *)realloc(p_sentence->data, word_count * sizeof(struct word *));
-            printf("\np_word=%p\n", p_word+word_count);
-            printf("p_sentence=%p\n", p_sentence);
-            printf("p_sentence->data=%p\n\n\n", p_sentence->data+word_count);
-            p_sentence->data[word_count] = *p_word;
-            p_sentence->word_count = word_count; 
             word_start = word_end + 1;
         }
         if(*p == '.') {
+            p_sentence = realloc(p_sentence, (sentence_count+1) * sizeof(struct sentence*));
+            if(p_sentence == NULL) {
+                printf("realloc p_sentence fails\n");
+                return doc;
+            }
+            p_sentence[sentence_count].data = p_word;
+            p_sentence[sentence_count].word_count = word_count;
+            
             word_count = 0;
             sentence_count++;
             
-            if(p_paragraph == NULL){
-                p_paragraph = calloc(1, sizeof(struct paragraph*));
-            }
-            printf("get_document enter 999999\n");
-            printf("sentence_count=%d\n", sentence_count);
-            p_paragraph->data = (struct sentence *)realloc(p_paragraph->data, sentence_count * sizeof(struct sentence *));
-            printf("p_paragraph=%p\n", p_paragraph);
-            p_paragraph->data[sentence_count] = *p_sentence;
-            p_paragraph->sentence_count = sentence_count;
 
             p_word = NULL;
-            p_sentence = NULL;
+            
         }
         if(*p == '\n') {
+            p_paragraph = realloc(p_paragraph, (paragraph_count+1) * sizeof(struct paragraph*));
+            if(p_paragraph == NULL) {
+                printf("realloc p_sentence fails\n");
+                return doc;
+            }
+            p_paragraph[paragraph_count].data = p_sentence;
+            p_paragraph[paragraph_count].sentence_count = sentence_count;
             sentence_count = 0;
             paragraph_count++;
-            word_start ++; /* skip '\n' for new word*/
-            printf("222");
-            doc.data = (struct paragraph *)realloc(doc.data, paragraph_count * sizeof(struct paragraph *));
-            
-            doc.data[paragraph_count] = *p_paragraph;
-            doc.paragraph_count = paragraph_count;
-            p_paragraph = NULL;
+            p_sentence = NULL;
+            word_start+= 1;
         }
         p++;
     }
     /*The last paragraph which is not end of '\n'*/
-    printf("111");
+    p_paragraph = realloc(p_paragraph, (paragraph_count+1) * sizeof(struct paragraph*));
+    if(p_paragraph == NULL) {
+        printf("realloc p_sentence fails\n");
+        return doc;
+    }
+    p_paragraph[paragraph_count].data = p_sentence;
+    p_paragraph[paragraph_count].sentence_count = sentence_count;
+    
     paragraph_count ++;
-    doc.data = (struct paragraph *)realloc(doc.data, paragraph_count * sizeof(struct paragraph *));
-    doc.data[paragraph_count] = *p_paragraph;
+    
+    doc.data= p_paragraph;
     doc.paragraph_count = paragraph_count;
 
     return doc;
 }
 
+void print_doc(struct document doc){
+    for(int i = 0; i < doc.paragraph_count; i++){
+        struct paragraph* p_paragraph = &doc.data[i];
+        
+        for(int j = 0; j < p_paragraph->sentence_count; j++){
+            struct sentence* p_sentence = &p_paragraph->data[j];
+            for(int k = 0; k < p_sentence->word_count; k++){
+                struct word* p_word = &p_sentence->data[k];
+                printf("%s ", p_word->data);
+            }
+            printf(".");
+        }
+        printf("\n");
+    }
+}
+
+void free_doc(struct document doc){
+    for(int i = 0; i < doc.paragraph_count; i++){
+        struct paragraph* p_paragraph = &doc.data[i];
+        
+        for(int j = 0; j < p_paragraph->sentence_count; j++){
+            struct sentence* p_sentence = &p_paragraph->data[j];
+            for(int k = 0; k < p_sentence->word_count; k++){
+                struct word* p_word = &p_sentence->data[k];
+                printf("k=%d, p_word->data=%p\n", k, p_word->data);
+                free(p_word->data);
+            }
+            //free(p_sentence->data);
+        }
+        //free(p_paragraph->data);
+    }
+}
+
 int main()
 {
     char *s = "Learning C is fun.\nLearning pointers is more fun.It is good to have pointers.";
-    
-    printf("hello\n");
     struct document doc = get_document(s);
-    // printf("doc.paragraph_count=%d\n", doc.paragraph_count);
-    // for(int i = 0; i < doc.paragraph_count; i++){
-    //     printf("sentence_count of paragraph%d is %d\n", i, doc.data[i].sentence_count);
-    // }
-
+    print_doc(doc);
+    // free_doc(doc);
     return 0;
 }
